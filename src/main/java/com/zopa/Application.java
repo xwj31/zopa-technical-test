4d2da31ede905e0ca5c8edbdef62e8da2fe7e197
xwj31 <>
2020-01-13 14:13:38 +0000

    rough first pass of solution

--- src/main/java/com/zopa/Application.java
+++ src/main/java/com/zopa/Application.java
@@ -5,6 +5,7 @@ import com.zopa.model.LoanQuote;
 import com.zopa.reader.CsvReader;
 import picocli.CommandLine;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 @CommandLine.Command(name = "ratecalculator", description = "Calculate the best " +
@@ -19,6 +20,9 @@ public class Application implements Runnable {
     private int loanAmount;
 
     public List<List<LoanQuote>> completeLoanQuoteSet = new ArrayList<>();
+    public double monthlyRepayment;
+    public double totalRepayment;
+    public BigDecimal rate;
 
     public static void main(String... args) {
         System.exit(new CommandLine(new Application()).execute(args));
@@ -37,14 +41,23 @@ public class Application implements Runnable {
                 //sort the input list so the lowest rate is processed first
                 loanQuotes.sort(Comparator.comparing(LoanQuote::getRate));
 
-                loanQuotes.stream().findFirst().orElseThrow(LenderRetrievalError::new);
-                minChange(loanQuotes, new ArrayList<>());
+                findRequestedTotalLoanByLeastLenders(loanQuotes, new ArrayList<>());
 
-                // find the lowest rate for each loan set
-                for (List<LoanQuote> loanQuoteSet: completeLoanQuoteSet) {
-                    loanQuoteSet.forEach(LoanQuote::calculateQuote);
+                List<LoanQuote> anotherList = completeLoanQuoteSet
+                        .stream()
+                        .findFirst()
+                        .orElseThrow(LenderRetrievalError::new);
 
+                anotherList.forEach(LoanQuote::calculateQuote);
+
+                monthlyRepayment = anotherList.stream().mapToDouble(LoanQuote::getMonthlyRepayment).sum();
+                totalRepayment = anotherList.stream().mapToDouble(LoanQuote::getTotalRepayment).sum();
+                double sum = 0.0;
+                for (LoanQuote loanQuote : anotherList) {
+                    double loanQuoteRate = loanQuote.getRate().doubleValue();
+                    sum += loanQuoteRate;
                 }
+                rate = BigDecimal.valueOf(sum /anotherList.size());
 
             } else {
                 System.out.println("Please enter an amount between 1000 and 15000");
@@ -54,19 +67,18 @@ public class Application implements Runnable {
         }
 
         System.out.println("Requested amount: " +loanAmount);
-        //System.out.println("Rate " + rateSum);
-        //System.out.println("Monthly repayment: £" + monthlyRepayment);
-        //System.out.println("Total repayment: £" + totalRepayment);
-
+        System.out.println("Rate " + rate);
+        System.out.println("Monthly repayment: £" + monthlyRepayment);
+        System.out.println("Total repayment: £" + totalRepayment);
     }
 
-    public void minChange(List<LoanQuote> loanQuoteList, List<LoanQuote> workingList) {
+    public void findRequestedTotalLoanByLeastLenders(List<LoanQuote> loanQuoteList, List<LoanQuote> workingList) {
         for (int i = 0; i < loanQuoteList.size(); i++) {
             List<LoanQuote> list = new ArrayList<>(workingList);
             list.add(loanQuoteList.get(i));
 
             //recursive call
-            minChange(loanQuoteList.subList(i + 1, loanQuoteList.size()), list);
+            findRequestedTotalLoanByLeastLenders(loanQuoteList.subList(i + 1, loanQuoteList.size()), list);
         }
 
         int cumulativeAmount = 0;
